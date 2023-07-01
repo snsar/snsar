f5e35081889727e5a478a2e6c0fae72a26a61b37
snsar <71001237+snsar@users.noreply.github.com>
2023-07-01 10:19:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const axios = require('axios');
 const fs = require("fs");
 
 const limit = 1;
-const apiURL = `https://api.api-ninjas.com/v1/jokes?limit=${limit}`;
+const apiURL = `https://api.api-ninjas.com/v1/facts?limit=${limit}`;
 
-const getJoke = async () => {
+const getFact = async () => {
   try {
     const response = await axios.get(apiURL, {
       headers: {
         'X-Api-Key': 'iemkezsN5DXWbWCaGxZU2g==VWckCXz0dmoeK9Ux'
       }
     })
-    const joke = response.data[0].joke;
+    const fact = response.data[0].fact;
 
 
-    console.log("new joke", `"${joke}"`);
+    console.log("new fact", `"${fact}"`);
 
     return {
-      joke,
+      fact,
     };
   } catch (err) {
     console.error(err.message);
@@ -26,13 +26,13 @@ const getJoke = async () => {
 };
 
 const generate = async () => {
-  const { joke } = await getJoke();
+  const { fact } = await getFact();
 
-  if (!joke) return;
+  if (!fact) return;
 
   const heading = '# Hi, I\'m Snsar!';
   const imageMarkdown = '<img src="https://acegif.com/wp-content/uploads/2021/4fh5wi/pepefrg-21.gif" alt="drawing"  height = "100"/>';
-  const readmeContent = `${heading} ${imageMarkdown} <br> <br> ${joke}`;
+  const readmeContent = `${heading} ${imageMarkdown} <br> <br> ${fact}`;
 
   fs.writeFileSync('README.md', readmeContent);
 };
