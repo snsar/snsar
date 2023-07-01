780d55a8e9b1b7eb327038538906d2495eb1cb77
snsar <71001237+snsar@users.noreply.github.com>
2023-07-01 10:13:43 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const generate = async () => {
   if (!joke) return;
 
   const heading = '# Hi, I\'m Snsar!';
-  const imageMarkdown = '![Pepe Frog](https://acegif.com/wp-content/uploads/2021/4fh5wi/pepefrg-21.gif "Pepe Frog")';
+  const imageMarkdown = '<img src="https://acegif.com/wp-content/uploads/2021/4fh5wi/pepefrg-21.gif" alt="drawing"  height = "100"/>';
   const readmeContent = `${heading} ${imageMarkdown} <br> <br> ${joke}`;
 
   fs.writeFileSync('README.md', readmeContent);
